6b85db827cbd731f65a2cce0184e7654509a7d9e
Marcello Del Buono <m.delbuono@gmail.com>
2021-10-01 22:20:27 +0200

    Updated plugin to stilo 0.15.0 format

--- index.js
+++ index.js
@@ -4,15 +4,12 @@ const Server = exports.Server = require('./lib/server');
 
 exports.stilo = {
     
-    commands: {
-        
-        viewer (store, options={}) {
-            const server = Server(store.homeStore);
-            const port = options.port || 8010;
-            server.listen(port, err => {
-                if (err) throw err;
-                console.log(`oloViewer server listening on port ${port}`);
-            });
-        }
+    viewer (store, options={}) {
+        const server = Server(store.homeStore);
+        const port = options.port || 8010;
+        server.listen(port, err => {
+            if (err) throw err;
+            console.log(`oloViewer server listening on port ${port}`);
+        });
     }
 };
